eb9088d000983f29c3ada117c83cd5bdfecadd05
justas <justas.dtemp@gmail.com>
2018-06-06 19:44:50 +0300

    gdpr module symlink

--- update.sh
+++ update.sh
@@ -129,6 +129,9 @@ ln -s /home/oxidbase/data/modules/translations/.gitignore modules/translations/.
 rm -r modules/shopinn
 ln -s /home/oxidbase/data/modules/shopinn/ modules/shopinn
 
+rm -r modules/gdpr
+ln -s /home/oxidbase/data/modules/gdpr/ modules/gdpr
+
 /bin/rm payment.inc.php
 ln -s /home/oxidbase/data/payment.inc.php payment.inc.php
 
