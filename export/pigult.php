a2e6ffd801df2daec7b933471c6f267b0ef20da5
Justas Jokubauskas <justas.j@datadog.org>
2018-07-13 07:52:38 +0000

    pigult

---
+++ export/pigult.php
@@ -0,0 +1,12 @@
+<?php
+
+//error_reporting(E_ALL);
+//ini_set('error_display', 1);
+ini_set("memory_limit", "1024M");
+set_time_limit(0);
+
+require_once dirname(dirname(__FILE__)) . "/bootstrap.php";
+
+/** @var Export $obj */
+$obj = oxNew("Export");
+$obj->run('pigult');
